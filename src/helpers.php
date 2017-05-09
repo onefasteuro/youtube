ba45a27bb8ed4e61507f42fddcaae6178e18143d
Oliver <oliver@bpisports.net>
2017-05-09 16:52:50 -0400

    added endpoints

--- src/helpers.php
+++ src/helpers.php
@@ -15,11 +15,25 @@ class Ytube
         $this->base_url = 'https://www.googleapis.com/youtube/v3/';
     }
 
-
-
-    public function search($query)
+    private function formatUrl($endpoint, $query = [])
     {
-        $url = $this->base_url . 'search?' . $query .'&key='.$this->key;
+        $query['key'] = $this->key;
+        $query = http_build_query($query);
+        $url = $this->base_url . $endpoint . '?' . $query;
+        return $url;
+    }
+
+
+    public function search($query = [])
+    {
+        $url = $this->formatUrl('search', $query);
+        $response = Requests::get($url);
+        return json_decode($response->body);
+    }
+
+    public function videos($query = [])
+    {
+        $url = $this->formatUrl('videos', $query);
         $response = Requests::get($url);
         return json_decode($response->body);
     }
