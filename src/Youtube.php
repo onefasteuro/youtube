6c6b152d337d9baa69d80fd8eb551c1b1122565f
Oliver <oliver@bpisports.net>
2017-05-10 10:37:45 -0400

    added gitignore and get video channel

--- src/Youtube.php
+++ src/Youtube.php
@@ -40,5 +40,15 @@ class Youtube
         return json_decode($response->body);
     }
 
+
+    public function channelVideos($channel, $query = [])
+    {
+        order=date&part=snippet&channelId={channel id here}&maxResults=25&key={YOUR_API_KEY}
+        $url = $this->formatUrl('search', $query);
+        $url .= '&channelId='.$channel.'&part=snippet&order=date';
+        $response = Requests::get($url);
+        return json_decode($response->body);
+    }
+
 }
 
